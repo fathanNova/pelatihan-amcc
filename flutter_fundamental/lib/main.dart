e1fad7adb4aff5f80cce5a5f1abd6f322c083773
fathanNova <novaaroyan13@gmail.com>
2025-05-30 22:58:13 +0700

    update

--- flutter_fundamental/lib/main.dart
+++ flutter_fundamental/lib/main.dart
@@ -14,8 +14,85 @@ class MyApp extends StatelessWidget{
         appBar: AppBar(
           title: const Text('Flutter Fundamental AMCC'),
         ),
+        body: const HomePage(),
       ),
     );
   }
 }
 
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+  String _displayText = '';
+
+  void _updateText(){
+    setState(() {
+      _displayText = _controller.text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Masukan teks dibawah:',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Ketik sesuatu...',
+            ),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+              onPressed: _updateText,
+              child: const Text('Tampilkan Teks'),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Output: $_displayText',
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'List Data:',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            child: ListView(
+              children: List.generate(
+                  5,
+                  (index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Item ${index + 1}'),
+                        const Icon(Icons.check_circle, color: Colors.green),
+                      ],
+                    ),
+                  ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
